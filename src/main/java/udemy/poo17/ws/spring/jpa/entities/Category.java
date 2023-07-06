b7cb700e999848c98411e853a922a08a2809c2c3
MatosOliveira <matosalem.oliveira@gmail.com>
2023-07-06 18:59:51 -0300

    Product and Many-to-many association with JoinTable

--- src/main/java/udemy/poo17/ws/spring/jpa/entities/Category.java
+++ src/main/java/udemy/poo17/ws/spring/jpa/entities/Category.java
@@ -4,12 +4,17 @@
 package udemy.poo17.ws.spring.jpa.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +28,10 @@ public class Category implements Serializable {
 	private Integer id;
 	
 	private String name;
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 	}
@@ -47,6 +56,10 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public Set<Product> getProducts() {
+		return products;
+	}
 
 	@Override
 	public int hashCode() {
@@ -64,5 +77,5 @@ public class Category implements Serializable {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
